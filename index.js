e19b2f00603e60288dfd00871176e2ad767ad784
Omar sabry <omarsabry@gmail.com>
2025-03-28 16:49:51 +0200

    Create App

---
+++ index.js
@@ -0,0 +1,32 @@
+ const xmpp = require('simple-xmpp')
+ 
+ xmpp.on('online', data => {
+   console.log('You are online')
+   console.log(`connected as ${data.jid.user}`)
+ })
+ 
+ xmpp.on('error', error => console.log(`something went wrong! ${error}`))
+ 
+ xmpp.on('chat', (from, message) =>
+   console.log(`Got a message ${message} from ${from}`)
+ )
+ 
+ xmpp.connect({
+   jid: 'admin@localhost',
+   password: 'password',
+   host: 'localhost',
+   port: 5222
+ })
+ 
+ function send (){
+     setTimeout(send,5000 )
+     xmpp.send("omar@localhost",`hi ${Date.now()}`)
+ }
+
+
+ git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:Omar-Sa6ry/XMPP.git
+git push -u origin main
---
+++ omar.js
@@ -0,0 +1,26 @@
+const xmpp = require('simple-xmpp')
+
+xmpp.on('online', data => {
+  console.log('You are online')
+  console.log(`connected as ${data.jid.user}`)
+
+  send()
+})
+
+xmpp.on('error', error => console.log(`something went wrong! ${error}`))
+
+xmpp.on('chat', (from, message) =>
+  console.log(`Got a message ${message} from ${from}`)
+)
+
+xmpp.connect({
+  jid: 'omar@localhost',
+  password: 'password',
+  host: 'localhost',
+  port: 5222
+})
+
+function send () {
+  setTimeout(send, 5000)
+  xmpp.send('admin@localhost', `hi ${Date.now()}`)
+}
